fb7d6afc88dde0290c4fb6d14848077232243691
DanteDeMeyer <demeyer.dante@gmail.com>
2023-04-19 09:32:41 +0000

    fine fune iperf2 script
    I've corrected the errors in the iperf3 script. So I've added a dollar sign before arguments that were missing one and I've deleted the first check for all the arguments.
    I did this because when you want to you tcp instead of udp you don't need to write something. I my previous code this would give a problem, because it checked for all the arguments.

--- scripts/iperf3.sh
+++ scripts/iperf3.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
-# Check for arguments
-if [ "$#" -lt 6 ]; then
-    echo "Error: Not enough arguments provided. Usage: $0 <ip addr> <duration> <UDP/TCP> <log_file> <bandwidth> <Uplink/downlink>"
-    exit 1
-fi
+
 # Validate input
 if ! ping -t1 -c 1 "$1" > /dev/null 2>&1; then
     echo "Error: Invalid IP address provided."
@@ -18,15 +14,15 @@ fi
 # Define variables
 ip_addr=$1
 duration=$2
-protocol=$3
-log_file_name=$(echo "$4" | sed 's/\.[^.]*$//')
+protocol=$6
+log_file_name=$(echo "$3" | sed 's/\.[^.]*$//')
 log_file=/home/dante/logs/$log_file_name.csv
-bandwidth=$5
-direction=$6
+bandwidth=$4
+direction=$5
 
 # Run the iperf3 and add timestamp
 for (( i=1; i<=$duration; i++ ))
 do
-    iperf3 -c "$ip_addr" "protocol" -b  "$direction" "bandwitdh" -t 1 -J | jq -r '.start.timestamp.timesecs as $start | .intervals[] | [(($start | tonumber), (.streams[0].bits_per_second/1000000), (.streams[0].jitter_ms))] | @csv'| awk -F, '{print strftime("%Y-%m-%d %H:%M:%S", $1), $2, $3}' >> "$log_file"
+    iperf3 -c "$ip_addr" "$protocol" -b  "$direction" "$bandwitdh" -t 1 -J | jq -r '.start.timestamp.timesecs as $start | .intervals[] | [(($start | tonumber), (.streams[0].bits_per_second/1000000), (.streams[0].jitter_ms))] | @csv'| awk -F, '{print strftime("%Y-%m-%d %H:%M:%S", $1), $2, $3}' >> "$log_file"
 done
 echo iperf3 successfully done for $2 seconds to $1
