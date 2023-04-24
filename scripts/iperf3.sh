79b19f46e19d31fed0e58f844916c2d71fcdaba9
DanteDeMeyer <demeyer.dante@gmail.com>
2023-04-24 15:09:38 +0000

    simplifying script

--- scripts/iperf3.sh
+++ scripts/iperf3.sh
@@ -6,27 +6,14 @@ if ! ping -t1 -c 1 "$1" > /dev/null 2>&1; then
     exit 1
 fi
 
-if ! [[ "$2" =~ ^[0-9]+$ ]]; then
-    echo "Error: Duration must be a positive integer."
-    exit 1
-fi
-
 # Define variables
 ip_addr=$1
-duration=$2
-protocol=$6
 log_file_name=$(echo "$3" | sed 's/\.[^.]*$//')
 log_file=/home/dante/logs/$log_file_name.csv
-bandwidth=$4
-direction=$5
 
 # Run the iperf3 command for the specified duration, with a sleep of 1 second between each iteration
-for (( i=1; i<=$duration; i++ ))
-do
     # Run iperf3 and extract timestamp and bitrate using jq, then append to log file
-    iperf3 -c "$ip_addr" "$protocol" -b  "$bandwitdh" "$direction" -t 1 -J | jq -r '.start.timestamp as $start | .intervals[] | [($start.timesecs | strftime("%Y-%m-%d %H:%M:%S")), (.streams[0].bits_per_second/1000000)] | @csv' >> "$log_file"
-    sleep 1
-done
+    iperf3 -c "$ip_addr" -u -b  100m -R -J | jq -r '.start.timestamp as $start | .end | [($start.timesecs | strftime("%Y-%m-%d %H:%M:%S")), (.streams[0].bits_per_second/1000000), (.streams[0].jitter_ms)] | @csv' >> "$log_file"
 
 # Print completion message
-echo iperf3 successfully done for $2 seconds to $1 saved in "$log_file"
+echo iperf3 successfully to $1 saved in "$log_file"
