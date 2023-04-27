25567d6cdc0813483ad2c5a316f58c91fd3137fc
DanteDeMeyer <demeyer.dante@gmail.com>
2023-04-27 20:23:56 +0200

    added explanation of the code

--- scripts/runscripts.py
+++ scripts/runscripts.py
@@ -1,11 +1,14 @@
 import subprocess
 
 while True:
+    # Ask the user to enter a number to choose a script
     script_num = input("Which script do you want to run?\nPress 1 for iperf3.sh\nPress 2 for ping_latency.sh\nPress 3 for quectelRM500q.py\nPress 4 for setupQuectelRM500q.py\nPress 5 for totaliperf3.py\nEnter a number from 1 to 5 (type exit to quit): ")
 
+    # Type exit to break the loop and terminate the program
     if script_num.lower() == 'exit':
         break
 
+    # The entered number will run the script, it will also prompt for a measurement number.
     if script_num in ['1', '2', '3', '4', '5']:
         if script_num == '1':
             measurement = input("Enter measurement number: ")
